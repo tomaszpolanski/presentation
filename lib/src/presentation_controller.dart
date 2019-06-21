fa8feeda97b49fb765dbf345f7aaadf14507f41b
PakoSt <kokotekilata@gmail.com>
2019-06-21 06:17:33 +0100

    Fix for MacOS keyboard support (#1)
    * Fix MacOS keyboard support
    
    - Add a separate method to handle raw keyboard events per platform
    - Add Mac platform
    - Add Linux platform
    
    Notes:
    Both Linux and Windows see the events as RawKeyEventDataAndroid at the moment.
    
    * Fix MacOS keyboard support v2
    
    - Use method to return keyCode as int with default value
    - Use switch statement to return the keyCode from PlatformData
    - Remove duplicated code handling the RawKeyCodes

--- lib/src/presentation_controller.dart
+++ lib/src/presentation_controller.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-const pageUpKey = 266;
-const pageDownKey = 267;
-const leftKey = 263;
-const rightKey = 262;
-const downKey = 264;
-const upKey = 265;
+class AndroidKeys {
+  static const pageUpKey = 266;
+  static const pageDownKey = 267;
+  static const leftKey = 263;
+  static const rightKey = 262;
+  static const downKey = 264;
+  static const upKey = 265;
+}
+
+class MacOSKeys {
+  static const escapeKey = 53;
+  static const spaceKey = 49;
+  static const pageUpKey = 116;
+  static const pageDownKey = 121;
+  static const leftKey = 123;
+  static const rightKey = 124;
+  static const downKey = 125;
+  static const upKey = 126;
+}
+
 
 class PresentationController {
   PresentationController({@required this.controller})
@@ -33,28 +47,51 @@ class PresentationController {
 
   void _handleKey(RawKeyEvent value) {
     if (value is RawKeyUpEvent) {
-      if (value.data is RawKeyEventDataAndroid) {
-        final RawKeyEventDataAndroid data = value.data;
-        print(data.keyCode);
-        switch (data.keyCode) {
-          case 20:
-          case 21:
-          case leftKey:
-          case pageUpKey:
-          case upKey:
-            _sendAction(PageAction.previous);
-            break;
-          case 19:
-          case 22:
-          case rightKey:
-          case pageDownKey:
-          case downKey:
-            _sendAction(PageAction.next);
-            break;
-          default:
-            break;
-        }
+      print(value.data);
+      _handleRawKeys(_returnKeyCode(value));
+    }
+  }
+
+  int _returnKeyCode(RawKeyEvent value){
+    switch (value.data.runtimeType){
+        case RawKeyEventDataLinux:
+          final RawKeyEventDataLinux data = value.data;
+          return data.keyCode;
+        case RawKeyEventDataMacOs:
+          final RawKeyEventDataMacOs data = value.data;
+          return data.keyCode;
+        case RawKeyEventDataAndroid:
+          final RawKeyEventDataAndroid data = value.data;
+          return data.keyCode;
+        default:
+          return -1;
       }
+  }
+
+  void _handleRawKeys(int keyCode){
+    switch (keyCode) {
+      case 20:
+      case 21:
+      case AndroidKeys.leftKey:
+      case AndroidKeys.pageUpKey:
+      case AndroidKeys.upKey:
+      case MacOSKeys.leftKey:
+      case MacOSKeys.pageUpKey:
+      case MacOSKeys.upKey:
+        _sendAction(PageAction.previous);
+        break;
+      case 19:
+      case 22:
+      case AndroidKeys.rightKey:
+      case AndroidKeys.pageDownKey:
+      case AndroidKeys.downKey:
+      case MacOSKeys.rightKey:
+      case MacOSKeys.pageDownKey:
+      case MacOSKeys.downKey:
+        _sendAction(PageAction.next);
+        break;
+      default:
+        break;
     }
   }
 
