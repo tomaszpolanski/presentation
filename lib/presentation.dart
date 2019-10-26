b6a49e3e90ba0cef36497dc0c19f2f8069d4775d
Tomek Polański <polanski.tomek@gmail.com>
2019-10-26 19:58:16 +0100

    Frame and editor (#4)
    * Adding AppFrame
    
    * WIP

--- lib/presentation.dart
+++ lib/presentation.dart
@@ -6,4 +6,6 @@ export 'package:presentation/src/presentation_page.dart';
 export 'package:presentation/src/presentation_stepper.dart';
 export 'package:presentation/src/widgets/animation_builder.dart';
 export 'package:presentation/src/widgets/animation_mode.dart';
+export 'package:presentation/src/widgets/app_frame.dart';
+export 'package:presentation/src/widgets/editor.dart';
 export 'package:presentation/src/widgets/parallax_background.dart';
---
+++ lib/src/utils/split.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+
+Iterable<InlineSpan> splitMapJoin(
+  String data,
+  Pattern pattern, {
+  InlineSpan Function(Match match) onMatch,
+  Iterable<InlineSpan> Function(String nonMatch) onNonMatch,
+}) sync* {
+  if (pattern is! Pattern) {
+    throw ArgumentError('$pattern is not a Pattern');
+  }
+
+  int startIndex = 0;
+  for (final match in pattern.allMatches(data)) {
+    yield* onNonMatch(data.substring(startIndex, match.start));
+    yield onMatch(match);
+    startIndex = match.end;
+  }
+  yield* onNonMatch(data.substring(startIndex));
+}
